9a56c119ac5dd5467734c90440b4ba282ba31d97
Nshoan Abdlwafa <nshoan.abdlwafa@chasacademy.com>
2022-11-08 20:16:47 +0100

    third commit

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-whatever();
+myList();
 
-function whatever() {
-  return fetch("data.json")
+function myList() {
+  return fetch(
+    "https://raw.githubusercontent.com/ChasAcademy-Nshoan-Abdlwafa/JSON-Todo-List/main/data.json"
+  )
     .then((result) => result.json())
     .then((content) => {
       console.log(content);
     });
 }
+
+let list = document.querySelector("ul");
+list.addEventListener(
+  "click",
+  function (ev) {
+    if (ev.target.tagName === "LI") {
+      ev.target.classList.toggle("checked");
+    }
+  },
+  false
+);
