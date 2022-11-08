e39105fff4684f4c4f96a24edcde9ed76367950a
Nshoan Abdlwafa <nshoan.abdlwafa@chasacademy.com>
2022-11-08 22:20:01 +0100

    fourth commit

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
-myList();
-
-function myList() {
+let list = document.getElementById("list");
+appendList();
+/* let x = list[8]; */
+function appendList() {
   return fetch(
     "https://raw.githubusercontent.com/ChasAcademy-Nshoan-Abdlwafa/JSON-Todo-List/main/data.json"
   )
-    .then((result) => result.json())
+    .then((result) => result.json()) // skapa JSON-data, "jsonifiera"
     .then((content) => {
-      console.log(content);
+      // färdigt JSON-objekt att använda
+      /* Skapa for-loop, som går igenom varje element i listan
+      första delen: var ska man börja iterera, här börjar vi på allra första elementet, dvs. i = 0
+      andra delen: när ska vi sluta, här slutar vi när vi har nått sista elementet
+      tredje delen: hur många steg per iteration, här vill vi iterera över varje enskild listelement, så i++ (i += 1)*/
+      for (let i = 0; i < content.todolist.length; i++) {
+        // Skapa upp ett li-objekt i html, via javascript
+        let listItem = document.createElement("li");
+
+        // populera skapade li-elementet med värdet från ditt JSON-data
+        let todoListItem = content.todolist[i];
+        listItem.innerHTML = todoListItem.ToDo;
+
+        if (todoListItem.completed) {
+          listItem.classList.add("checked");
+          listItem.classList.add("checkmark");
+        }
+
+        // lägg till ditt li-element till din ul-lista, som du hämtade in från HTML-dokumentet
+        list.appendChild(listItem);
+      }
     });
 }
-
-let list = document.querySelector("ul");
-list.addEventListener(
-  "click",
-  function (ev) {
-    if (ev.target.tagName === "LI") {
-      ev.target.classList.toggle("checked");
-    }
-  },
-  false
-);
